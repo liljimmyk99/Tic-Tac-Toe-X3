cf6e64433ec009124834678f66c69402b1653eba
Jimmy Kane <jbk5603@psu.edu>
2022-07-18 13:13:30 -0400

    Working prototype

--- GO/main.go
+++ GO/main.go
@@ -5,9 +5,11 @@ import (
 )
 
 var board [3][3]string
+var winner bool
 
 func main() {
 	fmt.Println("Welcome to Tic-Tac-Toe")
+	winner = false
 	board = [3][3]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}}
 	printBoard(board)
 	startGame()
@@ -16,19 +18,31 @@ func main() {
 
 func startGame() {
 	fmt.Println("X's go First")
-	xTurn()
-	oTurn()
-	printBoard(board)
+	for !winner {
+		xTurn()
+		if winner {
+			continue
+		}
+		oTurn()
+	}
+
 }
 
 func xTurn() {
 	choice := getUserChoice()
 	chooseSpace(choice, "X")
+	printBoard(board)
+	if checkWin() {
+		fmt.Println("Congrats you have won!")
+	}
 }
 
 func oTurn() {
 	choice := getUserChoice()
 	chooseSpace(choice, "O")
+	if checkWin() {
+		fmt.Println("Congrats you have won!")
+	}
 }
 
 func chooseSpace(choice int, letter string) {
@@ -126,4 +140,55 @@ func getUserChoice() int {
 	var choice int
 	fmt.Scanln(&choice)
 	return choice
+}
+
+func checkWin() bool {
+	/*
+
+		Ways to win: across the rows, down the row, or diagonal
+		across [1, 2, 3][4, 5, 6][7,8,9]
+		down [1,4,7][2,5,8],[3,6,9]
+		diagonal[1,5,9][3,5,7]
+	*/
+	if checkAcross() || checkDown() || checkDiagonal() {
+		winner = true
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func checkDiagonal() bool {
+	if (board[0][0] == board[1][1]) && (board[0][0] == board[2][2]) {
+		return true
+	} else if (board[0][2] == board[1][1]) && (board[0][2] == board[2][0]) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func checkDown() bool {
+	if (board[0][0] == board[1][0]) && (board[0][0] == board[2][0]) {
+		return true
+	} else if (board[0][1] == board[1][1]) && (board[0][1] == board[2][1]) {
+		return true
+	} else if (board[0][2] == board[1][2]) && (board[0][2] == board[2][2]) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func checkAcross() bool {
+	if (board[0][0] == board[0][1]) && (board[0][0] == board[0][2]) {
+		return true
+	} else if (board[1][0] == board[1][1]) && (board[1][0] == board[1][2]) {
+		return true
+	} else if (board[2][0] == board[2][1]) && (board[2][0] == board[2][2]) {
+		return true
+	} else {
+		return false
+	}
 }
