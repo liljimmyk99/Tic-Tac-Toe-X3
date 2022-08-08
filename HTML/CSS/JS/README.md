400162c1262d324ea1670e30dd1b3ab78a192669
Jimmy Kane <jbk5603@psu.edu>
2022-08-08 13:03:37 -0400

    Inital fully working prototype and to-do list

---
+++ HTML/CSS/JS/README.md
@@ -0,0 +1,7 @@
+# To Do
+1. Style the buttons
+2. Give the background something better looking
+3. Style the text for side and font size
+4. Center on screen
+5. Celebration lockout, confetti & Sound
+6. Mobile/iPad screen optizmation????? (Dynamic sizing)
--- HTML/CSS/JS/game.js
+++ HTML/CSS/JS/game.js
@@ -2,7 +2,11 @@
 // https://stackoverflow.com/questions/10000083/javascript-event-handler-with-parameters
 var game = {
     turn: "X",
-    board: [[],[],[]],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ],
     winner: false
 }
 function goesFirst(button){
@@ -16,12 +20,73 @@ function goesFirst(button){
     dqs("#game-board").style.visibility = 'visible'
 }
 
-function selected(letter, event) {
+function selected(button) {
     // When button is selected, have the letter be placed inside
-    // Make it kn
+    button.innerHTML = game.turn
+    let row = button.dataset.row
+    let col = button.dataset.column
+    button.disabled = "disabled"
+    game.board[row][col] = game.turn
+    console.log(game.board)
+    changeTurn()
+    checkWinner()
 }
 
+function changeTurn(){
+    if (game.turn === "X"){
+        game.turn = "O"
+    } else {
+        game.turn = "X"
+    }
+    dqs("#msg").innerHTML=`It is ${game.turn}'s turn`
+}
+
+
+
+function checkWinner(){
+    if (checkAcross() || checkDown() || checkDiagonal()) {
+		game.winner = true;
+        console.log("Winner")
+        dqs("body").style.background = "purple"
+        return true
+	} else {
+		return false
+	}
+}
+
+function checkDiagonal(){
+    if(game.board[0][0] === game.board[1][1] && game.board[0][0] == game.board[2][2] && game.board[0][0] !== null){
+        return true
+    } else if (game.board[0][2] === game.board[1][1] && game.board[0][2] == game.board[2][0] && game.board[0][2] !== null){
+        return true
+    } else {
+        return false
+    }
+}
+
+function checkDown(){
+    if (game.board[0][0] === game.board[1][0] && game.board[0][0] === game.board[2][0] && game.board[0][0] !== null) {
+		return true
+	} else if (game.board[0][1] === game.board[1][1] && game.board[0][1] === game.board[2][1] && game.board[0][1] !== null) {
+		return true
+	} else if (game.board[0][2] === game.board[1][2] && game.board[0][2] === game.board[2][2] && game.board[0][2] !== null) {
+		return true
+	} else {
+		return false
+	}
+}
+
+function checkAcross()  {
+	if (game.board[0][0] === game.board[0][1] && game.board[0][0] === game.board[0][2] && game.board[0][0] !== null) {
+		return true
+	} else if (game.board[1][0] === game.board[1][1] && game.board[1][0] === game.board[1][2] && game.board[1][0] !== null) {
+		return true
+	} else if (game.board[2][0] === game.board[2][1] && game.board[2][0] === game.board[2][2] && game.board[2][0] !== null) {
+		return true
+	} else {
+		return false
+	}
+}
 function dqs(target){
-    console.log(document.querySelector(target))
     return document.querySelector(target)
 }
