55281e3f752641591c6c8ac9987c5e2797a3196e
Jimmy Kane <jbk5603@psu.edu>
2022-08-08 11:04:06 -0400

    Logic for inital start

--- HTML/CSS/JS/game.js
+++ HTML/CSS/JS/game.js
@@ -1,11 +1,19 @@
 
-function goesFirst(event){
-    console.log("Clicked")
+// https://stackoverflow.com/questions/10000083/javascript-event-handler-with-parameters
+var game = {
+    turn: "X",
+    board: [[],[],[]],
+    winner: false
+}
+function goesFirst(button){
     // Get the button clicked and know if it is X or O
-    console.log(event)
+    let letter = button.innerHTML
     // Save this state somewhere to this application
-
+    game.turn = letter;
     // Show the board and have all the buttons, onHover show the letter going first
+    dqs("#btn-group").remove()
+    dqs("#msg").innerHTML=`It is ${letter}'s turn`
+    dqs("#game-board").style.visibility = 'visible'
 }
 
 function selected(letter, event) {
@@ -17,11 +25,3 @@ function dqs(target){
     console.log(document.querySelector(target))
     return document.querySelector(target)
 }
-
-function init(){
-    Window.onload = () =>{
-    document.querySelector("#x-button").addEventListener("click",() => console.log("kkkkkclickedkkkkkk"))
-    document.querySelector("#o-button").addEventListener("click", event => goesFirst)
-    }
-}
-init()
