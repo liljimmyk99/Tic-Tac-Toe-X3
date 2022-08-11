732bae1becc338bad24a6f9c7e2bd70e39b4ffb0
Jimmy Kane <jbk5603@psu.edu>
2022-08-10 23:15:50 -0400

    working on hover and removing event listner

--- HTML/CSS/JS/game.js
+++ HTML/CSS/JS/game.js
@@ -1,4 +1,4 @@
-
+// import ConfettiGenerator from "confetti-js";
 // https://stackoverflow.com/questions/10000083/javascript-event-handler-with-parameters
 var game = {
     turn: "X",
@@ -9,6 +9,16 @@ var game = {
     ],
     winner: false
 }
+
+/*
+let buttons = document.querySelectorAll('.game-settings')
+for (button of buttons){
+    console.log(button)
+    button.addEventListener('click', goesFirst(button))
+}
+
+*/
+
 function goesFirst(button){
     // Get the button clicked and know if it is X or O
     let letter = button.innerHTML
@@ -20,16 +30,21 @@ function goesFirst(button){
     dqs("#game-board").style.visibility = 'visible'
 }
 
-function selected(button) {
+function selected(buttonID, buttonRow, buttonColumn) {
     // When button is selected, have the letter be placed inside
+    dqs(buttonID).disabled = "disabled"
+    dqs(buttonID).removeEventListener("mouseenter", mouseHover(button), true)
+    dqs(buttonID).removeEventListener("mouseleave", mouseLeave(button), true)
+    
+    game.board[buttonRow][buttonColumn] = game.turn
     button.innerHTML = game.turn
-    let row = button.dataset.row
-    let col = button.dataset.column
-    button.disabled = "disabled"
-    game.board[row][col] = game.turn
+    button.innerHTML = game.turn
     console.log(game.board)
-    changeTurn()
-    checkWinner()
+    if(!checkWinner()){
+        changeTurn()
+    }
+    
+   
 }
 
 function changeTurn(){
@@ -41,12 +56,12 @@ function changeTurn(){
     dqs("#msg").innerHTML=`It is ${game.turn}'s turn`
 }
 
-
-
 function checkWinner(){
     if (checkAcross() || checkDown() || checkDiagonal()) {
 		game.winner = true;
         console.log("Winner")
+        let confetti = new ConfettiGenerator(dqs('body'))
+        confetti.render()
         dqs("body").style.background = "purple"
         return true
 	} else {
@@ -89,4 +104,16 @@ function checkAcross()  {
 }
 function dqs(target){
     return document.querySelector(target)
+}
+
+function mouseHover(btn){
+    btn.addEventListener("mouseenter", () => {
+        btn.innerHTML = game.turn
+    })
+}
+
+function mouseLeave(btn){
+    btn.addEventListener("mouseleave", () => {
+        btn.innerHTML = ""
+    })
 }
